49866ffd5ca7aa63175faab08999d0c494c71776
Eber Nar <eberrdz679@gmail.com>
2021-11-11 20:35:43 -0600

    callback hell

--- async/callback.js
+++ async/callback.js
@@ -1,11 +1,21 @@
 function hola(nombre, miCallback){
     setTimeout(function(){
         console.log('Hola '+ nombre);
-        miCallback();
+        miCallback(nombre);
+    },1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios', nombre);
+        otroCallback();
     },1000);
 }
 
 console.log('Iniciando proceso...');
-hola('Eber', function(){
-    console.log('Terminando proceso...');
+hola('Eber', function(nombre){
+    adios(nombre, function(){
+        console.log('Terminando proceso');
+    });
+    
 });
