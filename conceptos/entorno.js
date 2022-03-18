e309200c6b28da4a667d34838d5e75565e080042
nar alvarado <EberNarGit>
2022-03-17 19:53:35 -0600

    nodemon funcinal

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -2,4 +2,4 @@ let nombre = process.env.NOMBRE || "No hay nombre";
 let web = process.env.WEB || "No hay web";
 
 console.log('Hola ' + nombre);
-console.log('mi web es ' + web);
+console.log('Mi web es ' + web);
